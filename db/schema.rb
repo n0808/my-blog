b51f44b01b1761dfdbc4ba0848a9b2847155e88a
n0808 <life.with.chums.119@gmail.com>
2020-08-04 11:53:46 +0900

    新規登録ログインレイアウト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_102948) do
+ActiveRecord::Schema.define(version: 2020_08_03_075108) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,13 +19,21 @@ ActiveRecord::Schema.define(version: 2020_07_31_102948) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "profils", force: :cascade do |t|
+  create_table "profiles", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "name", default: "", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_profils_on_user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
+
+  # create_table "profils", force: :cascade do |t|
+  #   t.integer "user_id", null: false
+  #   t.string "name", default: "", null: false
+  #   t.datetime "created_at", precision: 6, null: false
+  #   t.datetime "updated_at", precision: 6, null: false
+  #   t.index ["user_id"], name: "index_profils_on_user_id"
+  # end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,5 +47,5 @@ ActiveRecord::Schema.define(version: 2020_07_31_102948) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "profils", "users"
+  add_foreign_key "profiles", "users"
 end
